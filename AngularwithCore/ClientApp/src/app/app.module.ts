cd34506955ecf3013e21145157438ad029190fba
ved prakash sharma <vedprakashsharma@gmail.com>
2020-08-19 22:30:40 +0530

    login css or code commit

--- AngularwithCore/ClientApp/src/app/app.module.ts
+++ AngularwithCore/ClientApp/src/app/app.module.ts
@@ -3,17 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
+//import { NavMenuComponent } from './nav-menu/nav-menu.component';
+//import { HomeComponent } from './home/home.component';
 import { AdminModule} from './admin/admin.module';
 import { CustomerModule } from './customer/customer.module';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent
+    LoginComponent,
+    //NavMenuComponent,
+    //HomeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -22,7 +23,7 @@ import { CustomerModule } from './customer/customer.module';
     AdminModule,
     CustomerModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
+      { path: '', component: LoginComponent, pathMatch: 'full' }
     ])
   ],
   providers: [],
